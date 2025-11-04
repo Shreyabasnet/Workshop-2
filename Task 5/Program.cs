81fc31bae9f88c2c4c675c477770dbfd017dde5d
Shreyabasnet <basnets349@gmail.com>
2025-11-04 15:17:29 +0545

    changes

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿DateTime birthDate = new DateTime(2003, 5, 12);  
+
+DateTime currentDate = DateTime.Now;
+
+TimeSpan ageSpan = currentDate - birthDate;
+int ageInYears = (int)(ageSpan.TotalDays / 365.25);  
+
+DateTime newDate = birthDate.AddDays(10);
+
+Console.WriteLine($"Your Birthdate: {birthDate.ToShortDateString()}");
+Console.WriteLine($"Current Date: {currentDate.ToShortDateString()}");
+Console.WriteLine($"Your Age: {ageInYears} years");
+Console.WriteLine($"Birthdate after adding 10 days: {newDate.ToShortDateString()}");
